1393cc86a4ccd96d7ad32052ac5532ad004814e1
Andrew Canaan <andrewtcanaan@gmail.com>
2022-07-12 16:59:41 -0400

    first example
    example of getting a set of stocks from stock_info and then getting the respective alpha vantage data

--- think tank/exercises.py
+++ think tank/exercises.py
@@ -1,1 +1,27 @@
+from pandas_datareader import data as web
+from yahoo_fin import stock_info as si
+from pandas import ExcelWriter
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import datetime
+import time
+import os
 
+yf.pdr_override()
+pd.options.display.width = 0
+
+start_date = datetime.datetime.now() - datetime.timedelta(days = 365)
+end_date = datetime.date.today()
+
+tickers_nasdaq = si.tickers_nasdaq()
+tickers_nasdaq = [item.replace(".", "-") for item in tickers_nasdaq] # yahoo finance prefers dashes in the tickers, not periods
+index_name = '^IXIC'  # S&P 500: ^GSPC  Dow Jones: ^DJI   Nasdaq: ^IXIC
+
+for item in tickers_nasdaq:
+    # Set a time delay between each stock
+    time.sleep(0.01)
+
+    # df = web.get_data_yahoo(item, start_date, end_date) # https://pandas-datareader.readthedocs.io/en/latest/remote_data.html
+    df = web.DataReader(item, "av-daily", start_date, end_date, api_key = os.getenv('TJOCDKQ1PCX3BW7T'))
+    print(df.head())

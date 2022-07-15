94c55e8048239e983643c6902d5197ff6cc8dc8e
acanaan <andrewtcanaan@gmail.com>
2022-07-14 20:51:22 -0400

    updated criteria for sigma

--- think tank/api-demo.py
+++ think tank/api-demo.py
@@ -3,13 +3,13 @@ from yahoo_fin import stock_info as si
 import requests
 import pandas as pd
 
-# tickers = si.tickers_sp500()
-# tickers = tickers[0:20]
+tickers = si.tickers_sp500()
+tickers = tickers[0:20]
 
-# url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={tickers[1]}&apikey=TJOCDKQ1PCX3BW7T'
-# response = requests.get(url)
-# data = response.json()
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={tickers[0]}&apikey=TJOCDKQ1PCX3BW7T'
+response = requests.get(url)
+data = response.json()
 
-# df = data.json_normalize()
+df = pd.json_normalize(data)
 
-# print(data.head(10))
+print(df.head(10))

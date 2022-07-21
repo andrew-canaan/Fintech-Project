8db7a6a6cbd9242028cb75f3e7743ab1b75564ea
andrew.canaan <andrew.canaan@us.af.mil>
2022-07-21 13:33:24 -0400

    try to start on getting AV to work

--- think tank/misc/api-demo.py
+++ think tank/misc/api-demo.py
@@ -2,20 +2,15 @@
 from yahoo_fin import stock_info as si
 import pandas as pd
 import requests
+import json
 
 pd.set_option('display.width', 160)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
-tickers = si.tickers_sp500()
+tickers = si.tickers_nasdaq()
 
 # JSON Version:
+# for at home: https://stackoverflow.com/questions/67277838/convert-alphavantage-api-response-to-dataframe
 url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={tickers[0]}&apikey=TJOCDKQ1PCX3BW7T'
 response = requests.get(url)
-
-try:
-    data = response.json()
-    df = pd.DataFrame.from_dict(data, orient = "index")
-    df.to_csv(f'{tickers[0]}.csv')
-except Exception as e:
-    print(e)

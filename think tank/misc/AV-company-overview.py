893505dafa52efea2535c2a93c06b74a594dff02
andrew.canaan <andrew.canaan@us.af.mil>
2022-07-26 14:10:52 -0400

    company overview demo complete

--- think tank/misc/AV-company-overview.py
+++ think tank/misc/AV-company-overview.py
@@ -11,18 +11,13 @@ pd.set_option('display.max_rows', None)
 
 # JSON Version:
 # for at home: https://stackoverflow.com/questions/67277838/convert-alphavantage-api-response-to-dataframe
-url = f'https://www.alphavantage.co/query?function=EARNINGS&symbol=IBM&apikey=TJOCDKQ1PCX3BW7T'
+url = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=TJOCDKQ1PCX3BW7T'
 response = requests.get(url)
 
 data = json.loads(response.text)
 
-annual_earnings_data = pd.DataFrame(data['annualEarnings'])
-quarterly_earnings_data = pd.DataFrame(data['quarterlyEarnings'])
+overview_data = pd.DataFrame.from_dict(data, orient = 'index', columns = ['Value'])
 
-writer = ExcelWriter("annual-earnings-demo.xlsx")
-annual_earnings_data.to_excel(writer, "Output")
-writer.save()
-
-writer = ExcelWriter("quarterly-earnings-demo.xlsx")
-quarterly_earnings_data.to_excel(writer, "Output")
+writer = ExcelWriter("company-overview-demo.xlsx")
+overview_data.to_excel(writer, "Output")
 writer.save()

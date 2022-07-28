a0c41a8945c5a5f2eabb085f35b50cd4285d6971
andrew.canaan <andrew.canaan@us.af.mil>
2022-07-28 08:30:13 -0400

    total active etf/stock list complete!

--- think tank/misc/AV-active-listings.py
+++ think tank/misc/AV-active-listings.py
@@ -3,27 +3,37 @@ from yahoo_fin import stock_info as si
 from pandas import ExcelWriter
 import pandas as pd
 import requests
-import json
 import csv
 
 pd.set_option('display.width', 160)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
+# https://requests.readthedocs.io/en/latest/user/advanced/
 # If no date is set, the API endpoint will return a list of active or delisted symbols as of the latest trading day
 # By default, state=active and the API will return a list of actively traded stocks and ETFs. Set state=delisted to query a list of delisted assets
 CSV_URL = 'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=demo'
 
+exportList = pd.DataFrame(columns=['Symbol', 'Name', 'Exchange', 'Asset Type'])
+
+# with requests.Session() as s:
+#     download = s.get(CSV_URL)
+#     decoded_content = download.content.decode('utf-8')
+
+#     df = pd.read_csv(decoded_content, delimiter = ',')
+#     print(df.tail(1))
+
 with requests.Session() as s:
     download = s.get(CSV_URL)
-
     decoded_content = download.content.decode('utf-8')
-
-    read_file = pd.read_csv (r'Path where the CSV file is stored\File name.csv')
-    read_file.to_excel (r'Path to store the Excel file\File name.xlsx', index = None, header=True)
 
     cr = csv.reader(decoded_content.splitlines(), delimiter=',')
     my_list = list(cr)
-    for row in my_list:
-        print(row)
 
+    for row in my_list:
+        if (len(row[0]) <= 4):
+            exportList = exportList.append({'Symbol': row[0], 'Name': row[1], 'Exchange': row[2], 'Asset Type': row[3]}, ignore_index = True)
+
+writer = ExcelWriter("AV-active-listings.xlsx")
+exportList.to_excel(writer, "Output")
+writer.save()
